9814d91946f618fb882fe3fc27d6921065dcc4b2
wzy <2639809653@qq.com>
2026-05-16 17:38:14 +0800

    feat: configure OpenAPI documentation
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ springboot/src/main/java/com/example/config/OpenApiConfig.java
@@ -0,0 +1,38 @@
+package com.example.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "BearerAuth";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("85Boss 智能招聘平台 API")
+                        .version("1.0.0")
+                        .description("集成 AI 简历筛选 + 协同过滤推荐的招聘平台")
+                        .contact(new Contact()
+                                .name("85Boss Team")
+                                .url("https://github.com/Hfuuwzy/ERSbyAI"))
+                        .license(new License()
+                                .name("MIT License")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
